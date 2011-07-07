23c354474fabe5cddafe72e82a1f043f25743ed4
Scott C. Sells <scottcs+dev@gmail.com>
2011-07-06 17:13:20 -0700

    add love conf

---
+++ conf.lua
@@ -0,0 +1,21 @@
+function love.conf(t)
+	t.title = "Pud"
+	t.author = "SCx Games"
+	t.identity = "pud"
+	t.version = 72
+	t.screen.width = 1024
+	t.screen.height = 768
+	t.screen.fullscreen = false
+	t.screen.vsync = false
+	t.screen.fsaa = 0
+	t.modules.joystick = false
+	t.modules.audio = true
+	t.modules.keyboard = true
+	t.modules.event = true
+	t.modules.image = true
+	t.modules.graphics = true
+	t.modules.timer = true
+	t.modules.mouse = true
+	t.modules.sound = true
+	t.modules.physics = false
+end
