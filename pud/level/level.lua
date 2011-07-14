00066e795bdde2fd85a071f09d81d0f23d9d281b
Scott C. Sells <scottcs+dev@gmail.com>
2011-07-14 07:38:58 -0700

    fix . : silliness

--- pud/level/level.lua
+++ pud/level/level.lua
@@ -3,15 +3,27 @@ local MapBuilder = require 'pud.level.MapBuilder'
 -- Level
 local Level = Class{name='Level'}
 
+-- generate a standard roguelike map with rooms connected via hallways.
 function Level:generateStandard(builder)
-	assert(builder.is_a(MapBuilder))
+	assert(builder:is_a(MapBuilder))
 	
-	builder.init()
-	builder.makeRooms()
-	builder.connectRooms()
-	builder.cleanup()
+	builder:init()
+	builder:makeRooms()
+	builder:connectRooms()
+	builder:cleanup()
 
-	return builder.getMap()
+	return builder:getMap()
+end
+
+-- generate a cavernous map with large open spaces and rough walls.
+function Level:generateCavernous(builder)
+	assert(builder:is_a(MapBuilder))
+	
+	builder:init()
+	builder:makeCaverns()
+	builder:cleanup()
+
+	return builder:getMap()
 end
 
 -- the class
