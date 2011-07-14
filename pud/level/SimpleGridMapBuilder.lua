34c2896cda84815e6a18c25c99278c9cddfd00ee
Scott C. Sells <scottcs+dev@gmail.com>
2011-07-14 07:49:45 -0700

    cleanup

--- pud/level/SimpleGridMapBuilder.lua
+++ pud/level/SimpleGridMapBuilder.lua
@@ -4,20 +4,8 @@ local MapBuilder = require 'pud.level.MapBuilder'
 -- MapBuilder
 local SimpleGridMapBuilder = Class{name='SimpleGridMapBuilder',
 	inherits = MapBuilder,
-	function(self)
-		MapBuilder.construct(self)
-	end
+	function(self) MapBuilder.construct(self) end
 }
-
--- destructor
-function SimpleGridMapBuilder:destroy()
-	self._map = nil
-end
-
--- initialize the map
-function SimpleGridMapBuilder:init()
-	self._map = Map()
-end
 
 -- generate all the rooms
 function SimpleGridMapBuilder:makeRooms()
@@ -29,11 +17,6 @@ end
 
 -- perform any cleanup needed on the map
 function SimpleGridMapBuilder:cleanup()
-end
-
--- get the created map
-function SimpleGridMapBuilder:getMap()
-	return self._map
 end
 
 -- the class
