510080c48e94e2ceef080d3a9aaac0a8fe9366be
Scott C. Sells <scottcs+dev@gmail.com>
2011-07-21 11:19:37 -0700

    split events into separate classes, remove Event namespace

---
+++ pud/event/GameExitEvent.lua
@@ -0,0 +1,13 @@
+local Class = require 'lib.hump.class'
+local Event = require 'pud.event.Event'
+
+-- Game Exit - fires when the game has nothing more to do
+local GameExitEvent = Class{name='GameExitEvent',
+	inherits=Event,
+	function(self)
+		Event.construct(self, 'Game Exit Event')
+	end
+}
+
+-- the class
+return GameExitEvent
