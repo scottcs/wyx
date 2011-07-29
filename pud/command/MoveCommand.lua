29322596f9c4f225ad8e824d4d3672edb8c9c21d
Scott C. Sells <scottcs+dev@gmail.com>
2011-07-29 16:29:00 -0700

    add MoveCommand

---
+++ pud/command/MoveCommand.lua
@@ -0,0 +1,36 @@
+local Class = require 'lib.hump.class'
+local Command = require 'pud.command.Command'
+
+-- MoveCommand
+--
+local MoveCommand = Class{name='MoveCommand',
+	inherits=Command,
+	function(self, target, vector)
+		Command.construct(self, target)
+
+		if target.getStepSize then
+			local step = target:getStepSize()
+			if step then
+				self._vector = vector
+				self._vector.x = self._vector.x * step.x
+				self._vector.y = self._vector.y * step.y
+			end
+		end
+
+		self._vector = self._vector or vector
+	end
+}
+
+-- destructor
+function MoveCommand:destroy()
+	self._vector = nil
+	Command.destroy(self)
+end
+
+function MoveCommand:execute()
+	local pos = self._target:getPositionVector()
+	self._target:setPosition(pos + self._vector)
+end
+
+-- the class
+return MoveCommand
