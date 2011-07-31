ab5505824bc778189effd45c1f7dda1698020ee2
Scott C. Sells <scottcs+dev@gmail.com>
2011-07-31 14:53:01 -0700

    attempt to speed up color function in pud.loveutil

--- pud/loveutil.lua
+++ pud/loveutil.lua
@@ -55,7 +55,7 @@ do
 	local sbg = love.graphics.setBackgroundColor
 
 	local function color(r, g, b, a)
-		if type(r) == 'table' then r,g,b,a = unpack(r) end
+		if type(r) == 'table' then r,g,b,a = r[1],r[2],r[3],r[4] end
 
 		if    r <= 1 and r >= 0
 			and g <= 1 and g >= 0
