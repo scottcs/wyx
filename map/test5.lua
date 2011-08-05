a292a6b9513381db1090c746c9ed6335e3ab71f1
Scott C. Sells <scottcs+dev@gmail.com>
2011-08-05 10:51:55 -0700

    add zones to test maps

--- map/test5.lua
+++ map/test5.lua
@@ -4,22 +4,6 @@ map.name = 'Test5'
 map.author = 'scx'
 
 --[[
-	note: worn and torch wall types will only show up on horizontal walls.
-	vertical walls are created automatically where needed.
-
-  if you fail to provide an entry in this table for a glyph you used in the
-  map below, you will receive an error when you try to load the map in-game.
---]]
-map.glyphs = {
-  empty = '0',
-  floor = {'.', ',', '_', 'x'},  -- normal, worn, interior, rug
-  wall = {'#', '%', '*'},        -- normal, worn, torch
-  door = {'+', '-'},             -- shut, open
-  stair = {'<', '>'},            -- up, down
-  trap = {'^'},                  -- normal (possibly more later)
-}
-
---[[
    set to true if you do not want to specify which floor and wall tiles are
    different. the map generator will randomly change some normal map and floor
    tiles to look worn or have torches.
@@ -30,6 +14,24 @@ map.glyphs = {
    more random worn tiles).
 --]]
 map.handleDetail = false
+
+--[[
+	note: worn and torch wall types will only show up on horizontal walls.
+	vertical walls are created automatically where needed.
+
+  if you fail to provide an entry in this table for a glyph you used in the
+  map below, you will receive an error when you try to load the map in-game.
+
+	make sure each line inside the first curly brackets ends with a comma.
+--]]
+map.glyphs = {
+  empty = '0',
+  floor = {'.', ',', '_', 'x'},  -- normal, worn, interior, rug
+  wall = {'#', '%', '*'},        -- normal, worn, torch
+  door = {'+', '-'},             -- shut, open
+  stair = {'<', '>'},            -- up, down
+  trap = {'^'},                  -- normal (possibly more later)
+}
 
 --[[
    the map. can be any size. spaces matter, even at the beginning of the line.
@@ -44,5 +46,19 @@ map.map = [[
 #x#
 ###
 ]]
+
+--[[
+	if you want to set zones for the map, you have to count the coordinates out
+	(top left corner of the map is 1,1), and just define the top left and bottom
+	right corners of the zone (the zone includes its borders).
+
+	for example:
+	map.zones = {
+	  tavern = {14, 9, 20, 12},
+	}
+	-- the top left corner is (14, 9) and the bottom right is (20, 12).
+
+	make sure each line inside the first curly brackets ends with a comma.
+--]]
 
 return map
