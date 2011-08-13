a1aa9a164777fb62a9ba135355e681466d211fe3
Scott C. Sells <scottcs+dev@gmail.com>
2011-08-13 13:52:01 -0700

    uncomment currently implemented components

--- pud/entity/HeroFactory.lua
+++ pud/entity/HeroFactory.lua
@@ -8,20 +8,18 @@ local HeroFactory = Class{name='HeroFactory',
 	function(self)
 		EntityFactory.construct(self, 'hero')
 		self._renderLevel = 5
-		--[[
 		-- required components (can be parent classes)
 		self._requiredComponents = {
-			getClass 'pud.component.HealthComponent',
-			getClass 'pud.component.PositionComponent',
-			getClass 'pud.component.TimeComponent',
+			--getClass 'pud.component.HealthComponent',
+			--getClass 'pud.component.PositionComponent',
+			--getClass 'pud.component.TimeComponent',
 			getClass 'pud.component.GraphicsComponent',
-			getClass 'pud.component.InfoPanelComponent',
-			getClass 'pud.component.CombatComponent',
+			--getClass 'pud.component.InfoPanelComponent',
+			--getClass 'pud.component.CombatComponent',
 			getClass 'pud.component.CollisionComponent',
 			getClass 'pud.component.MotionComponent',
-			getClass 'pud.component.InputComponent',
+			--getClass 'pud.component.InputComponent',
 		}
-		]]--
 	end
 }
 
