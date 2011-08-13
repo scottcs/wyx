5fd23f4d0aa7dcf44c200ea1c2087d0d759a8801
Scott C. Sells <scottcs+dev@gmail.com>
2011-08-13 14:05:29 -0700

    set defaults for properties

--- pud/component/property.lua
+++ pud/component/property.lua
@@ -9,52 +9,52 @@ local get = function(prop)
 	return property[prop]
 end
 
----------------------------
--- the actual properties --
----------------------------
+-------------------------------------------------
+-- the actual properties and sensible defaults --
+-------------------------------------------------
 
 -- combat properties
-property.Attack            = 'Attack'
-property.Defense           = 'Defense'
-property.AttackBonus       = 'AttackBonus'
-property.DefenseBonus      = 'DefenseBonus'
+property.Attack            = 1
+property.Defense           = 1
+property.AttackBonus       = 0
+property.DefenseBonus      = 0
 
 -- health properties
-property.Health            = 'Health'
-property.MaxHealth         = 'MaxHealth'
-property.HealthBonus       = 'HealthBonus'
-property.MaxHealthBonus    = 'MaxHealthBonus'
+property.Health            = 1
+property.MaxHealth         = 1
+property.HealthBonus       = 0
+property.MaxHealthBonus    = 0
 
 -- collision properties
-property.Position          = 'Position'
+property.Position          = {x=1, y=1}
 
 -- graphics properties
-property.TileSet           = 'TileSet'
-property.TileCoords        = 'TileCoords'
-property.TileSize          = 'TileSize'
-property.Visibility        = 'Visibility'
+property.TileSet           = 'dungeon'
+property.TileCoords        = {x=1, y=1}
+property.TileSize          = 32
+property.Visibility        = 6
 
 -- time properties
-property.DefaultCost       = 'DefaultCost'
-property.AttackCost        = 'AttackCost'
-property.MoveCost          = 'MoveCost'
-property.Speed             = 'Speed'
-property.SpeedBonus        = 'SpeedBonus'
+property.DefaultCost       = 1
+property.AttackCost        = 1
+property.MoveCost          = 1
+property.Speed             = 1
+property.SpeedBonus        = 0
 
 -- weaknesses
-property.CrushWeakness     = 'CrushWeakness'
-property.SliceWeakness     = 'SliceWeakness'
-property.StabWeakness      = 'StabWeakness'
-property.FireWeakness      = 'FireWeakness'
+property.CrushWeakness     = 0
+property.SliceWeakness     = 0
+property.StabWeakness      = 0
+property.FireWeakness      = 0
 
 -- resistances
-property.CrushResistance   = 'CrushResistance'
-property.SliceResistance   = 'SliceResistance'
-property.StabResistance    = 'StabResistance'
-property.FireResistance    = 'FireResistance'
+property.CrushResistance   = 0
+property.SliceResistance   = 0
+property.StabResistance    = 0
+property.FireResistance    = 0
 
 -- status effects
-property.Combustable       = 'Combustable'
+property.Combustable       = false
 
 
 -- check for mistakes when this file is loaded
