906d551aa6b39d6928f5013e6b105dd5b4780087
Scott C. Sells <scottcs+dev@gmail.com>
2011-08-14 14:26:04 -0700

    change order of arguments

--- pud/event/EntityPositionEvent.lua
+++ pud/event/EntityPositionEvent.lua
@@ -5,29 +5,29 @@ local Event = getClass 'pud.event.Event'
 --
 local EntityPositionEvent = Class{name='EntityPositionEvent',
 	inherits=Event,
-	function(self, entity, from, to)
+	function(self, entity, to, from)
 		verifyClass('pud.entity.Entity', entity)
-		verify('vector', from, to)
+		verify('vector', to, from)
 
 		Event.construct(self, 'Entity Position Event')
 
 		self._entity = entity
-		self._from = from
 		self._to = to
+		self._from = from
 	end
 }
 
 -- destructor
 function EntityPositionEvent:destroy()
 	self._entity = nil
-	self._from = nil
 	self._to = nil
+	self._from = nil
 	Event.destroy(self)
 end
 
 function EntityPositionEvent:getEntity() return self._entity end
-function EntityPositionEvent:getOrigin() return self._from:clone() end
 function EntityPositionEvent:getDestination() return self._to:clone() end
+function EntityPositionEvent:getOrigin() return self._from:clone() end
 
 -- the class
 return EntityPositionEvent
