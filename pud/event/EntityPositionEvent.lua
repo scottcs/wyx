f0683d4636917240c5b22752f518fb58c517287a
Scott C. Sells <scottcs+dev@gmail.com>
2011-08-14 14:19:51 -0700

    Revert "change to use vector instead of positions"
    This reverts commit 58aa68ddf2f9fa8b641a36b5a812392efc3768d4.

--- pud/event/EntityPositionEvent.lua
+++ pud/event/EntityPositionEvent.lua
@@ -5,26 +5,29 @@ local Event = getClass 'pud.event.Event'
 --
 local EntityPositionEvent = Class{name='EntityPositionEvent',
 	inherits=Event,
-	function(self, entity, v)
+	function(self, entity, from, to)
 		verifyClass('pud.entity.Entity', entity)
-		verify('vector', v)
+		verify('vector', from, to)
 
 		Event.construct(self, 'Entity Position Event')
 
 		self._entity = entity
-		self._v = v
+		self._from = from
+		self._to = to
 	end
 }
 
 -- destructor
 function EntityPositionEvent:destroy()
 	self._entity = nil
-	self._v = nil
+	self._from = nil
+	self._to = nil
 	Event.destroy(self)
 end
 
 function EntityPositionEvent:getEntity() return self._entity end
-function EntityPositionEvent:getVector() return self._v:clone() end
+function EntityPositionEvent:getOrigin() return self._from:clone() end
+function EntityPositionEvent:getDestination() return self._to:clone() end
 
 -- the class
 return EntityPositionEvent
