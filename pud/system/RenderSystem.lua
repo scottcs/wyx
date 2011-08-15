f8f4819d6338d90f9af338e469d1e9b271d77c28
Scott C. Sells <scottcs+dev@gmail.com>
2011-08-15 15:29:26 -0700

    level -> depth

--- pud/system/RenderSystem.lua
+++ pud/system/RenderSystem.lua
@@ -8,7 +8,7 @@ local table_sort = table.sort
 local RenderSystem = Class{name='RenderSystem',
 	function(self)
 		self._registered = {}
-		self._levels = {}
+		self._depths = {}
 	end
 }
 
@@ -19,47 +19,47 @@ function RenderSystem:destroy()
 		self._registered[k] = nil
 	end
 	self._registered = nil
-	self._levels = nil
+	self._depths = nil
 end
 
 -- draw
 function RenderSystem:draw()
-	for i=#self._levels,1,-1 do
-		local level = self._levels[i]
-		for obj in self._registered[level]:listeners() do obj:draw() end
+	for i=#self._depths,1,-1 do
+		local depth = self._depths[i]
+		for obj in self._registered[depth]:listeners() do obj:draw() end
 	end
 end
 
--- make sure the given level exists
-function RenderSystem:_touchLevel(level)
-	if not self._registered[level] then
-		self._registered[level] = ListenerBag()
+-- make sure the given depth exists
+function RenderSystem:_touchDepth(depth)
+	if not self._registered[depth] then
+		self._registered[depth] = ListenerBag()
 
 		-- create sorted, unique array
 		local unique = {}
-		unique[level] = true
+		unique[depth] = true
 
-		for i,l in pairs(self._levels) do
+		for i,l in pairs(self._depths) do
 			unique[l] = true
-			self._levels[i] = nil
+			self._depths[i] = nil
 		end
 
-		for l in pairs(unique) do self._levels[#self._levels+1] = l end
-		table_sort(self._levels)
+		for l in pairs(unique) do self._depths[#self._depths+1] = l end
+		table_sort(self._depths)
 	end
 end
 
 -- register an object
-function RenderSystem:register(obj, level)
-	level = level or 1
-	verify('number', level)
-	self:_touchLevel(level)
-	self._registered[level]:push(obj)
+function RenderSystem:register(obj, depth)
+	depth = depth or 1
+	verify('number', depth)
+	self:_touchDepth(depth)
+	self._registered[depth]:push(obj)
 end
 
 -- unregister an object
 function RenderSystem:unregister(obj)
-	for _,l in pairs(self._levels) do
+	for _,l in pairs(self._depths) do
 		if self._registered[l] then self._registered[l]:pop(obj) end
 	end
 end
