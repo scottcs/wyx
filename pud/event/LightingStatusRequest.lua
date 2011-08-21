18fd067de7d99ab8cbb380a6cf0a71d46786f87e
Scott C. Sells <scottcs+dev@gmail.com>
2011-08-21 15:24:53 -0700

    add LightingStatusRequest

---
+++ pud/event/LightingStatusRequest.lua
@@ -0,0 +1,29 @@
+local Class = require 'lib.hump.class'
+local Event = getClass 'pud.event.Event'
+
+-- LightingStatusRequest
+--
+local LightingStatusRequest = Class{name='LightingStatusRequest',
+	inherits=Event,
+	function(self, entity)
+		if type(entity) ~= number then entity = entity:getID() end
+		verify('number', entity)
+		assert(EntityRegistry:exists(entity),
+			'LightingStatusRequest: entity %d does not exist', entity)
+
+		Event.construct(self, 'Lighting Status Request')
+
+		self._entity = entity
+	end
+}
+
+-- destructor
+function LightingStatusRequest:destroy()
+	self._entity = nil
+	Event.destroy(self)
+end
+
+function LightingStatusRequest:getEntity() return self._entity end
+
+-- the class
+return LightingStatusRequest
