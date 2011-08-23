e0d9e69fac349578bd2c0b591dbfc2b2d612ccfd
Scott C. Sells <scottcs+dev@gmail.com>
2011-08-23 16:32:26 -0700

    add some AttachmentComponents to hero

--- pud/entity/HeroEntityFactory.lua
+++ pud/entity/HeroEntityFactory.lua
@@ -19,6 +19,9 @@ local HeroEntityFactory = Class{name='HeroEntityFactory',
 			getClass 'pud.component.MotionComponent',
 			getClass 'pud.component.InputComponent',
 			getClass 'pud.component.ContainerComponent',
+			getClass 'pud.component.WeaponAttachmentComponent',
+			getClass 'pud.component.ArmorAttachmentComponent',
+			getClass 'pud.component.RingAttachmentComponent',
 		}
 	end
 }
