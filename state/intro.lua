a2a0d9a48f863488cbb9b746b5caf5cfa940f8f1
Scott C. Sells <scottcs+dev@gmail.com>
2011-08-29 09:49:01 -0700

    change formatting

--- state/intro.lua
+++ state/intro.lua
@@ -8,24 +8,21 @@
 
 local st = GameState.new()
 
-function st:init()
-end
+function st:init() end
 
 function st:enter(prevState)
 	print('intro')
 	GameState.switch(State.menu)
 end
 
-function st:leave()
-end
+function st:leave() end
 
-function st:update(dt)
-end
+function st:destroy() end
 
-function st:draw()
-end
+function st:update(dt) end
 
-function st:keypressed(key, unicode)
-end
+function st:draw() end
+
+function st:keypressed(key, unicode) end
 
 return st
--- state/shutdown.lua
+++ state/shutdown.lua
@@ -9,9 +9,21 @@
 
 local st = GameState.new()
 
+function st:init() end
+
 function st:enter(prevState)
 	print('shutdown')
 	love.event.push('q')
 end
+
+function st:leave() end
+
+function st:destroy() end
+
+function st:update(dt) end
+
+function st:draw() end
+
+function st:keypressed(key, unicode) end
 
 return st
