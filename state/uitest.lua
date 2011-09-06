a341dd9058340ed8cf4a09d891c48e1710b29c7b
Scott C. Sells <scottcs+dev@gmail.com>
2011-09-06 15:55:23 -0700

    add Slot and StickyButton tests

--- state/uitest.lua
+++ state/uitest.lua
@@ -10,22 +10,29 @@ local st = RunState.new()
 local Frame = getClass 'pud.ui.Frame'
 local Text = getClass 'pud.ui.Text'
 local Button = getClass 'pud.ui.Button'
+local StickyButton = getClass 'pud.ui.StickyButton'
 local TextEntry = getClass 'pud.ui.TextEntry'
 local Bar = getClass 'pud.ui.Bar'
+local Slot = getClass 'pud.ui.Slot'
 local Style = getClass 'pud.ui.Style'
 
 local n1Style = Style({font=GameFont.big, fgcolor=colors.WHITE, bgcolor=colors.DARKRED})
 local h1Style = Style({font=GameFont.big, fgcolor=colors.WHITE, bgcolor=colors.LIGHTRED})
 local a1Style = Style({font=GameFont.big, fgcolor=colors.WHITE, bgcolor=colors.RED})
-local n2Style = Style({font=GameFont.small, fgcolor=colors.WHITE, bgcolor=colors.DARKORANGE})
-local h2Style = Style({font=GameFont.small, fgcolor=colors.WHITE, bgcolor=colors.LIGHTORANGE})
-local a2Style = Style({font=GameFont.small, fgcolor=colors.WHITE, bgcolor=colors.ORANGE})
+local n2Style = Style({font=GameFont.small, fgcolor=colors.WHITE, bgcolor=colors.BLACK})
+local h2Style = Style({font=GameFont.small, fgcolor=colors.WHITE, bgcolor=colors.GREY30})
+local a2Style = Style({font=GameFont.small, fgcolor=colors.WHITE, bgcolor=colors.GREY60})
 local n3Style = Style({font=GameFont.small, fgcolor=colors.BLACK, bgcolor=colors.DARKYELLOW})
 local h3Style = Style({font=GameFont.small, fgcolor=colors.BLACK, bgcolor=colors.LIGHTYELLOW})
 local a3Style = Style({font=GameFont.small, fgcolor=colors.BLACK, bgcolor=colors.YELLOW})
 local n4Style = Style({font=GameFont.verysmall, fgcolor=colors.WHITE, bgcolor=colors.DARKPURPLE})
 local h4Style = Style({font=GameFont.verysmall, fgcolor=colors.WHITE, bgcolor=colors.LIGHTPURPLE})
 local a4Style = Style({font=GameFont.verysmall, fgcolor=colors.WHITE, bgcolor=colors.PURPLE})
+local inventorySlotStyle = Style({image=Image.interface})
+inventorySlotStyle:setImageQuad(0, 288, 40, 40)
+local itemNStyle = Style({image=Image.item, bgcolor=colors.GREY90})
+itemNStyle:setImageQuad(0, 96, 32, 32)
+local itemHStyle = itemNStyle:clone({bgcolor=colors.WHITE})
 local nBarStyle = Style({font=GameFont.verysmall, fgcolor=colors.BLUE, bgcolor=colors.DARKBLUE})
 local hBarStyle = Style({font=GameFont.verysmall, fgcolor=colors.LIGHTBLUE, bgcolor=colors.DARKBLUE})
 
@@ -47,8 +54,6 @@ function st:enter(prevState, nextState, ...)
 
 	local text1 = Text(20, 20, 560, 360)
 	text1:setNormalStyle(n2Style)
-	text1:setHoverStyle(h2Style)
-	text1:setActiveStyle(a2Style)
 	text1:setMargin(10)
 	text1:setMaxLines(10)
 	text1:setJustifyRight()
@@ -67,17 +72,20 @@ function st:enter(prevState, nextState, ...)
 		end
 	end)
 
-	local button2 = Button(10, 60, 100, n4Style:getFont():getHeight() + 8)
-	button2:setNormalStyle(n4Style)
-	button2:setHoverStyle(h4Style)
-	button2:setActiveStyle(a4Style)
-	button2:setText('Button 2')
-	button2:setCallback('l', function(mods)
-		if mods.shift then self._testFrame3:setText('it\'s-a-shift-a!')
-		elseif mods.ctrl then self._testFrame3:setText('ctrllllllllll!')
-		else self._testFrame3:setText('click!')
-		end
-	end)
+	local invImg = inventorySlotStyle:getImage()
+	local invW, invH = 40, 40
+	local slotW = invW+4
+	local slot1 = Slot(10, 60, invW, invH)
+	slot1:setNormalStyle(inventorySlotStyle)
+	local slot2 = Slot(10+slotW, 60, invW, invH)
+	slot2:setNormalStyle(inventorySlotStyle)
+	local slot3 = Slot(10+slotW*2, 60, invW, invH)
+	slot3:setNormalStyle(inventorySlotStyle)
+
+	local sbut1 = StickyButton(0, 0, 32, 32)
+	sbut1:setNormalStyle(itemNStyle)
+	sbut1:setHoverStyle(itemHStyle)
+	slot1:swap(sbut1)
 
 	local entry1 = TextEntry(10, 300, 300, n3Style:getFont():getHeight() + 8)
 	entry1:setNormalStyle(n3Style)
@@ -107,7 +115,9 @@ function st:enter(prevState, nextState, ...)
 	end)
 
 	text1:addChild(button1)
-	text1:addChild(button2)
+	text1:addChild(slot1)
+	text1:addChild(slot2)
+	text1:addChild(slot3)
 	text1:addChild(text2)
 	text1:addChild(entry1)
 	text1:addChild(bar1)
