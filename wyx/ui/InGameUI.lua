f686988d98d4567b5a15404368ab0a2610fc8606
Scott C. Sells <scottcs+dev@gmail.com>
2011-09-09 21:29:45 -0700

    add InGameUI

---
+++ wyx/ui/InGameUI.lua
@@ -0,0 +1,56 @@
+local Class = require 'lib.hump.class'
+local Style = getClass 'wyx.ui.Style'
+local Frame = getClass 'wyx.ui.Frame'
+local Text = getClass 'wyx.ui.Text'
+local Button = getClass 'wyx.ui.Button'
+local StickyButton = getClass 'wyx.ui.StickyButton'
+local TextEntry = getClass 'wyx.ui.TextEntry'
+local Bar = getClass 'wyx.ui.Bar'
+local Slot = getClass 'wyx.ui.Slot'
+local TooltipFactory = getClass 'wyx.ui.TooltipFactory'
+
+local colors=colors
+
+-- constants
+local BOTTOMPANEL_HEIGHT_MULT = 0.1146
+
+-- styles
+local panelStyle = Style({
+	bordersize = 4,
+	bordercolor = colors.GREY20,
+	bgcolor = colors.GREY10,
+})
+
+-- InGameUI
+-- The interface for the main game.
+local InGameUI = Class{name='InGameUI',
+	inherits=Frame,
+	function(self)
+		Frame.construct(self, 0, 0, WIDTH, HEIGHT)
+		self:_makeBottomPanel()
+	end
+}
+
+-- destructor
+function InGameUI:destroy()
+	self._bottomPanel:destroy()
+	self._bottomPanel = nil
+	Frame.destroy(self)
+end
+
+function InGameUI:getGameSize()
+	return WIDTH, HEIGHT - self._bottomPanel:getHeight()
+end
+
+function InGameUI:_makeBottomPanel()
+	local height = HEIGHT * BOTTOMPANEL_HEIGHT_MULT
+	local y = HEIGHT - height
+	local f = Frame(0, y, WIDTH, height)
+	f:setNormalStyle(panelStyle)
+
+	self._bottomPanel = f
+end
+
+
+-- the class
+return InGameUI
