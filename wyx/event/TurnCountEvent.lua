2f9de82a16ee59fe6b2516eb16101e4c1b9c4281
Scott C. Sells <scottcs+dev@gmail.com>
2011-09-10 23:13:00 -0700

    add TurnCountEvent

---
+++ wyx/event/TurnCountEvent.lua
@@ -0,0 +1,24 @@
+local Class = require 'lib.hump.class'
+local Event = getClass 'wyx.event.Event'
+
+-- TurnCountEvent
+--
+local TurnCountEvent = Class{name='TurnCountEvent',
+	inherits=Event,
+	function(self, turnCount)
+		verify('number', turnCount)
+		Event.construct(self, 'Turn Count Event')
+		self._turnCount = turnCount
+	end
+}
+
+-- destructor
+function TurnCountEvent:destroy()
+	self._turnCount = nil
+	Event.destroy(self)
+end
+
+function TurnCountEvent:getTurnCount() return self._turnCount end
+
+-- the class
+return TurnCountEvent
