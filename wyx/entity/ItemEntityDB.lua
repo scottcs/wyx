c41ffce7fddf2f1e520b31f7932b7b5b9ced6b54
Scott C. Sells <scottcs+dev@gmail.com>
2011-09-26 14:18:54 -0700

    change automatic entity names to be a little smarter

--- wyx/entity/ItemEntityDB.lua
+++ wyx/entity/ItemEntityDB.lua
@@ -2,6 +2,8 @@ local Class = require 'lib.hump.class'
 local EntityDB = getClass 'wyx.entity.EntityDB'
 local ItemEntityFactory = getClass 'wyx.entity.ItemEntityFactory'
 
+local format = string.format
+
 -- ItemEntityDB
 --
 local ItemEntityDB = Class{name='ItemEntityDB',
@@ -19,6 +21,24 @@ function ItemEntityDB:destroy()
 	EntityDB.destroy(self)
 end
 
+function ItemEntityDB:_processEntityInfo(info)
+	local name = info.name
+
+	if not name then
+		if info.family and info.kind then
+			if info.family == 'Ring' then
+				name = format('%s of %s', info.family, info.kind)
+			elseif info.family == 'Armor' then
+				name = format('%s %s', info.kind, info.family)
+			end
+		end
+	end
+
+	info.name = name or info.kind or 'UNKNOWN'
+
+	local ret = EntityDB._processEntityInfo(self, info)
+	return ret
+end
 
 -- the class
 return ItemEntityDB
