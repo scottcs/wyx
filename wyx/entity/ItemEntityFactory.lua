50bd0e45c707dd5096251d812b3dcaa865535dee
Scott C. Sells <scottcs+dev@gmail.com>
2011-09-29 16:04:53 -0700

    fix some item properties being set to defaults instead of 0

--- wyx/entity/ItemEntityFactory.lua
+++ wyx/entity/ItemEntityFactory.lua
@@ -1,6 +1,9 @@
 local Class = require 'lib.hump.class'
 local EntityFactory = getClass 'wyx.entity.EntityFactory'
 local depths = require 'wyx.system.renderDepths'
+local property = require 'wyx.component.property'
+
+local match = string.match
 
 -- ItemEntityFactory
 -- creates entities based on data files
@@ -21,6 +24,37 @@ local ItemEntityFactory = Class{name='ItemEntityFactory',
 -- destructor
 function ItemEntityFactory:destroy()
 	EntityFactory.destroy(self)
+end
+
+function ItemEntityFactory:_postProcess(entity)
+	EntityFactory._postProcess(self, entity)
+
+	-- set default values to 0 for non-bonus properties
+	local comps = entity:getComponentsByClass(
+		getClass('wyx.component.Component'))
+
+	local num = #comps
+	for i=1,num do
+		local comp = comps[i]
+		if not isClass('wyx.component.TimeComponent', comp) then
+			local toZero = {}
+
+			for p in pairs(comp._properties) do
+				local normal = match(p, '(.*)Bonus$')
+				if normal and property.isproperty(normal) then
+					toZero[#toZero+1] = normal
+				end
+			end
+
+			local num = #toZero
+			if num > 0 then
+				for i=1,num do
+					local p = toZero[i]
+					comp:_setProperty(p, 0)
+				end
+			end
+		end
+	end
 end
 
 -- the class
